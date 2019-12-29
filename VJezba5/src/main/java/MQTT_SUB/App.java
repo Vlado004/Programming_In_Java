bda3e5f9422253780fc510890bdd7414703de7a6
Mateo Mamut <vladom156@gmail.com>
2019-12-29 17:14:22 +0100

    Start of task 5

--- VJezba5/src/main/java/MQTT_SUB/App.java
+++ VJezba5/src/main/java/MQTT_SUB/App.java
@@ -1,9 +1,9 @@
 package MQTT_SUB;
 
 public class App {
-
     public static void main(String args[]) {
 
+        Subscriber sub = new Subscriber();
+        sub.run();
     }
-
 }
--- VJezba5/src/main/java/MQTT_SUB/Subscriber.java
+++ VJezba5/src/main/java/MQTT_SUB/Subscriber.java
@@ -1,9 +1,33 @@
 package MQTT_SUB;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import jdk.jfr.StackTrace;
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-public class Subscriber {
+public class Subscriber implements MqttCallback {
 
+    private MqttClient client;
 
+    public void connectionLost(Throwable cause) {
+        System.out.println("Connection lost because: " + cause);
+        System.exit(2);
+    }
 
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        System.out.println(String.format("[%s] %s", topic, new String(message.getPayload())));
+    }
+
+    public void run() {
+        try {
+            client = new MqttClient("tcp://localhost:5654", "Subscriber_Test");
+            client.subscribe("#");
+        } catch (MqttException e) {
+            System.out.println(StackTrace);
+            System.exit(1);
+        }
+    }
+
+    //Nepotrebno ali jer je interface moramo declare-at
+    public void deliveryComplete(IMqttDeliveryToken Token) {
+    }
 }
